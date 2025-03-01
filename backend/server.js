b370c2b863baa06a0f40729651725c5064c813d9
Alex Gómez López <alexgomezlopez@Ordenador-portatil-de-Alex.local>
2025-03-01 12:28:36 +0300

    Agregada ruta /api para verificar estado de la API

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,19 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// Ruta base para `/api`
+app.get("/api", (req, res) => {
+    res.json({ mensaje: "🚀 API de SafeWater en funcionamiento!" });
+});
+
 // Rutas
 app.use("/api/usuarios", usuariosRoutes);
 app.use("/api/auth", authRoutes);
 
 sequelize.sync({ alter: true }).then(() => {
   console.log("📊 Base de datos sincronizada");
-  app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
+  app.listen(PORT, "0.0.0.0", () => 
+    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`)
+  );
 });
+
